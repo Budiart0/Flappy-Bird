f34056adc60eb00b36e51fee0d6b510bbe43ae56
Budiart0 <budifootball@gmail.com>
2026-09-14 19:34:07 +0700

    Refactor bird class and adjust ground scroll speed

--- main.py
+++ main.py
@@ -9,10 +9,22 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Flappy Bird")
 
 ground_scroll = 0
-scroll_speed = 4
+scroll_speed = 2
 
 background_img = pygame.image.load('bglong.png')
-ground_img = pygame.image.load('ground.png')
+ground_img = pygame.transform.scale(pygame.image.load('ground.png'),(1400, 168))
+
+class Bird:
+    def __init__(self, x, y):
+        self.image = pygame.image.load('bird1.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+bird = Bird(100, height // 2)
 
 while True:
     for event in pygame.event.get():
@@ -24,6 +36,7 @@ while True:
     ground_scroll -= scroll_speed
     if abs(ground_scroll) > 35:
         ground_scroll = 0
+    bird.draw(screen)
     pygame.display.update()
     clock.tick(60)
 
